0bb8056c29fc5b1bc0cff85170243ba34fbd56f2
이지원 <dlwldnjs950@naver.com>
2023-08-10 14:13:41 +0900

    [D3] Title: 햄버거 다이어트, Time: 752 ms, Memory: 21,096 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class Solution {
 	
 	/**
-	 * 	# 5215_햄버거 다이어트_부분집합
+	 * 	# 5215_햄버거 다이어트_조합
 	 * 
 	 * 	[설명]
 	 * 햄버거의 맛은 유지하면서 정해진 칼로리 넘지 않는 햄버거 주문하기
@@ -29,10 +29,10 @@ public class Solution {
 	 * 
 	 * 	[풀이방법]
 	 * 인덱스 번호를 고르자
-	 * 부분집합을 구해서
+	 * 조합으로 풀면
+	 * 선택하는 개수에 따라 각각 조합을 구하고
 	 * 칼로리의 합 이하인지
-	 * 이하면 그중에 맛 점수 가장 높은 것! 
-	 * 공집합도 만들어지므로 주의!
+	 * 맛 점수는 이상인지 확인
 	 * 
 	 * */
 	
@@ -52,8 +52,7 @@ public class Solution {
 	static StringBuilder sb;
 	static StringTokenizer st;
 	
-	static int N, L, maxYam;
-	static boolean[] isSelected;
+	static int N, L, selectedIngreIdx[], maxYam;
 	static ingredient[] ingre;
 
 	public static void main(String[] args) throws IOException{
@@ -71,9 +70,6 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			L = Integer.parseInt(st.nextToken());
 			
-			// 각 재료의 선택 여부
-			isSelected = new boolean[N];
-			
 			// 각 재료 정보 저장하기
 			ingre = new ingredient[N];
 			for(int idx = 0; idx<N; idx++) {
@@ -83,38 +79,43 @@ public class Solution {
 			}
 			
 			maxYam = 0;
-			// 재료 선택
-			subset(0);
+			// 재료 1개 ~ N개 선택
+			// 부분집합 코드를 활용하거나 조합의 코드를 사용하거나
+			for (int howMany = 1; howMany <= N; howMany++) {
+				// 선택한 재료 인덱스 담을 배열
+				selectedIngreIdx = new int[howMany];
+				// 재료 선택하기
+				combination(0, 0, howMany);
+			}
 			sb.append(maxYam).append("\n");
 		}
 		System.out.println(sb);
 	}
-
-	// 부분 집합은 자리의 선택
-	static void subset(int cnt) {
-		int totalCal = 0;
-		int totalYam = 0;
-		if(cnt == N) {
-			// 여기서 이제 선택된 재료들의 합을 구하자
-			for(int idx = 0; idx<N; idx++) {
-				if(isSelected[idx]) {
-					totalCal+= ingre[idx].calorie;
-					totalYam+= ingre[idx].yammy;
-				}
+	
+	// 조합의 코드를 적어보자
+	
+	static void combination(int cnt, int start, int howMany) {
+		if(cnt == howMany) {
+			// 재료 다 선택했으면
+			// 선택한 재료의 조건 만족 여부 확인
+			int totalCal = 0;
+			int totalYam = 0;
+			for(int idx=0; idx<howMany; idx++) {
+				totalCal += ingre[selectedIngreIdx[idx]].calorie;
+				totalYam += ingre[selectedIngreIdx[idx]].yammy;
 			}
-
+			
 			// 제한 칼로리 이하면 맛점수 확인
-            // 공집합은 제외
-			if (totalCal <= L && totalCal != 0) {
+			if (totalCal <= L) {
 				if (maxYam < totalYam)
 					maxYam = totalYam;
 			}
 			return;
 		}
-		isSelected[cnt] = true;
-		subset(cnt+1);
-		isSelected[cnt] = false;
-		subset(cnt+1);
-
+		for(int idx = start; idx<N; idx++) {
+			selectedIngreIdx[cnt] = idx;
+			combination(cnt+1, idx+1, howMany);
+		}
 	}
+
 }

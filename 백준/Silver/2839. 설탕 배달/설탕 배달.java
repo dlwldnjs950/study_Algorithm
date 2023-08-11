637d8c40269b55f0fa6e8fef33dab0310e2d7c2f
이지원 <dlwldnjs950@naver.com>
2023-08-11 13:58:32 +0900

    [Silver IV] Title: 설탕 배달, Time: 72 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,21 +1,71 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+	
+	/**
+	 * 	# 2839_설탕 배달
+	 * 
+	 * 	[설명]
+	 * 설탕 N킬로그램 배달
+	 * 설탕 묶음은 3kg, 5kg
+	 * 최대한 적은 봉지를 들고가자
+	 * 
+	 * 	[입력]
+	 * ===> 배달할 설탕 무게 N
+	 * 
+	 * 	[출력]
+	 * 배달하는 봉지 최소 개수
+	 * (Nkg을 만들지 못한다면 -1 출력)
+	 * 
+	 * 	[풀이방법]
+	 * 완전 탐색으로 풀었군...
+	 * 무게를 인덱스로 갖는 배열!
+	 * 값으로는 봉지 개수를 가지게 된다.
+	 * */
+	
+	static BufferedReader br;
+	static StringBuilder sb;
+	static StringTokenizer st;
 
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
 		
-		int N=sc.nextInt();
+		// 배달할 설탕 무게 N 입력받기
+		int N = Integer.parseInt(br.readLine().trim());
+		int[] bongji = new int[N + 1];
 
-		for(int i=0; 3*i<=N; i++) {
-			for(int j=0; 5*j<=N;j++) {
-				if((5*j+3*i)==N) {
-					System.out.println(i+j);
-					return;
+		if (N >= 3)
+			bongji[3] = 1;
+		if (N >= 5)
+			bongji[5] = 1;
+		
+		for (int num = 6; num <= N; num++) {
+			// 무게가 5의 배수면 - 봉지 중에 더 무거운 것부터 개수를 확인하자~!
+			if (num % 5 == 0) {
+				bongji[num] = bongji[num - 5] + 1;
+			// 무게가 3의 배수면
+			} else if (num % 3 == 0) {
+				bongji[num] = bongji[num - 3] + 1;
+			// 3의 배수도, 5의 배수도 아닌데
+			} else {
+				// 이 전에 봉지의 조합이 있었다면, (예를 들어 8)
+				if (bongji[num - 3] != 0 && bongji[num - 5] != 0) {
+					bongji[num] = Math.min(bongji[num - 3], bongji[num - 5]) + 1;
 				}
 			}
 		}
-		System.out.println(-1);
+		if(bongji[N] == 0) {
+			sb.append(-1);
+		}else {
+			sb.append(bongji[N]);
+		}
+		
+		System.out.println(sb);
 	}
 
 }

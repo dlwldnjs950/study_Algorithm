6c0eb8fb6c10ec1e7f4f46639291a6ae16969c00
이지원 <dlwldnjs950@naver.com>
2023-08-13 22:58:56 +0900

    [Silver V] Title: 색종이, Time: 80 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -9,65 +9,57 @@ public class Main {
 	 * 	# 2563_색종이
 	 * 
 	 * 	[설명]
-	 * 가로세로 100인 정사각형 도화지
-	 * 그 위에 가로세로 10인 정사각형 색종이 평행하게 붙임
-	 * 한 장 또는 여러 장
-	 * 색종이가 붙은 영역의 넓이 구하기
+	 * 100*100인 흰색 도화지
+	 * 10*10인 검은색 색종이
 	 * 
-	 * 색종이는 항상 도화지 안쪽에 붙어있다
+	 * 색종이를 여러장 붙인 뒤 검은 영역의 넓이?
 	 * 
 	 * 	[입력]
-	 * ===> 색종이 수 N
-	 * (N 줄 입력)
-	 * ===> 자연수 2개 (공백 구분)
-	 * 		도화지왼쪽변~색종이왼쪽변거리 도화지아래쪽변~색종이아래쪽변거리
+	 * ===> 색종이의 수 N
+	 * (N줄의 입력)
+	 * ===> 색종이가 붙을 좌표 (색종이의 왼쪽아래 꼭짓점 기준)	 * 
 	 * 
 	 * 	[출력]
-	 * 색종이 영역 넓이
+	 * 검은 영역 넓이
 	 * 
 	 * 	[풀이방법]
-	 * 색종이가 겹쳐있는 부분은 다중 카운트하면 안됨
-	 * 색종이 한장 확인할 때, 카운팅했다는 표시를 하고
-	 * 다음 색종이 확인할 때, 이미 카운팅 했으면 카운트하지 않는다
+	 * 색종이가 붙은 좌표가 입력될 때 마다 검은 면적 카운트
+	 * 이미 카운트한 영역은 표시해두고 다시 카운트하지 않는다
 	 * 
 	 * */
 	
 	static BufferedReader br;
 	static StringBuilder sb;
 	static StringTokenizer st;
-	
-	static final int WHITESIZE = 100;
-	static final int BLACKSIZE = 10;
 
+	static final int WHITE = 100;	// 도화지 크기
+	static final int BLACK = 10;	// 색종이 크기
+	
 	public static void main(String[] args) throws IOException{
 
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
-		
-		// 색종이 몇장인지 N
+		// 색종이 수 N
 		int N = Integer.parseInt(br.readLine().trim());
-		// 각 칸 카운팅 여부 2차원 배열
-		boolean isCounted[][] = new boolean[WHITESIZE+1][WHITESIZE+1];
-
-		int totalCnt = 0;
+		// 카운트한 영역인지 표시할 배열
+		boolean[][] isCounted = new boolean[WHITE + 1][WHITE + 1];		
+		
+		int cnt = 0; // 면적 카운트
 		for (int loop = 0; loop < N; loop++) {
 			st = new StringTokenizer(br.readLine().trim());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			
-			// 시작 지점을 기준으로 색종이 크기만큼 확인
-			for (int row = x; row < x + BLACKSIZE; row++) {
-				for (int col = y; col < y + BLACKSIZE; col++) {
-					// 카운트하지 않은 칸이라면
-					if(!isCounted[row][col]) {
-						totalCnt++;
-						//카운트 했다고 표시
-						isCounted[row][col] = true;
+			int row = Integer.parseInt(st.nextToken());
+			int col = Integer.parseInt(st.nextToken());
+			for (int rIdx = row; rIdx < row + BLACK; rIdx++) {
+				for (int cIdx = col; cIdx < col + BLACK; cIdx++) {
+					// 아직 카운트하지 않은 칸이면 카운트하고 방문 표시
+					if (!isCounted[rIdx][cIdx]) {
+						cnt++;
+						isCounted[rIdx][cIdx] = true;
 					}
 				}
 			}
 		}
-		sb.append(totalCnt);
+		sb.append(cnt);
 		System.out.println(sb);
 	}
 

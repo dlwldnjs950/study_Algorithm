11515bff95cc3cb3286b4826da94ec88693a2802
이지원 <dlwldnjs950@naver.com>
2023-08-16 22:37:52 +0900

    [Silver II] Title: DFS와 BFS, Time: 188 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,73 +1,111 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+	
+	/**
+	 * 	# DFS와 BFS
+	 * 
+	 * 	[설명]
+	 * 주어진 그래프를 DFS로 탐색한 결과와
+	 * BFS로 탐색한 결과를 출력해라
+	 * 
+	 * 방문할 수 있는 정점이 여러 개인 경우에는
+	 * 		정점 번호가 작은 것부터 방문한다
+	 * 정점 번호는 1부터 N까지
+	 * 
+	 * 	[입력]
+	 * ===> 정점개수N 간선개수M 탐색시작할정점번호V (공백 구분)
+	 * 
+	 * 	[출력]
+	 * DFS 수행 결과 출력 후 
+	 * 다음줄에 BFS 수행 결과
+	 * 
+	 * 	[풀이방법]
+	 * 
+	 * */
+	
+	static BufferedReader br;
+	static StringBuilder sb;
+	static StringTokenizer st;
 
-	public static boolean[] visited;
-	public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+	static List<List<Integer>> graph = new ArrayList<>();
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException{
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		// 정점개수N 간선개수M 탐색시작할정점번호V (공백 구분)
+		st = new StringTokenizer(br.readLine().trim());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
 
-		int N = sc.nextInt(); // 정점 개수
-		int M = sc.nextInt(); // 간선 개수
-		int V = sc.nextInt(); // 탐색 시작 정점 번호
-
-		visited = new boolean[N + 1]; // 정점 번호를 인덱스 값으로 쓰기 위해 +1
-		for (int i = 0; i < N + 1; i++) { // 그래프 초기화
+		// 그래프 초기화
+		for (int i = 0; i < N + 1; i++) {	// 정점 번호가 1부터 시작됨
 			graph.add(new ArrayList<Integer>());
 		}
-
-		for (int i = 0; i < M; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
+		
+		// 간선 개수 M만큼 두 정점 번호를 입력 받는다
+		for(int idx =0; idx <M; idx++) {
+			st = new StringTokenizer(br.readLine().trim());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			graph.get(a).add(b);
 			graph.get(b).add(a);
 		}
-
-		dfs(V);
-		System.out.println();
-		visited = new boolean[N + 1];
-		bfs(V);
-	}	
-
-	public static void dfs(int v) {
-		visited[v] = true;
-		System.out.print(v + " ");
-		for (int i = 0; i < graph.get(v).size(); i++) {
-			graph.get(v).sort(new Comparator<Integer>() {
+		
+		// 번호가 작은 정점부터 방문해야 하므로 정렬한다
+		for(int idx=1; idx<=N; idx++) {
+			graph.get(idx).sort(new Comparator<Integer>() {
 				@Override
 				public int compare(Integer o1, Integer o2) {
+					// TODO Auto-generated method stub
 					return o1 - o2;
 				}
 			});
-			int x = graph.get(v).get(i);
-			if (!visited[x])
-				dfs(x);
+		}
+		
+		visited = new boolean[N+1];	// 정점번호 1부터 시작
+		DFS(V);
+		sb.append("\n");
+		visited = new boolean[N+1];	// 정점번호 1부터 시작
+		BFS(V);
+		
+		System.out.println(sb);
+	}
+	
+	private static void DFS(int v) {
+		visited[v] = true;
+		sb.append(v).append(" ");
+		for(int idx = 0; idx<graph.get(v).size(); idx++) {
+			int a = graph.get(v).get(idx);
+			if(!visited[a])	// 방문한 적 없으면 DFS 돌리기
+				DFS(a);
 		}
 	}
-
-	public static void bfs(int v) {
-		Queue<Integer> q = new LinkedList<>();
+	
+	private static void BFS(int v) {
+		Queue<Integer> q = new ArrayDeque<>();
+		// 큐에 넣으면서 방문 표시
 		q.offer(v);
 		visited[v] = true;
-		while (!q.isEmpty()) {
-			int x = q.poll();
-			System.out.print(x + " ");
-			for (int i = 0; i < graph.get(x).size(); i++) {
-				graph.get(x).sort(new Comparator<Integer>() {
-					@Override
-					public int compare(Integer o1, Integer o2) {
-						return o1 - o2;
-					}
-				});
-				int y = graph.get(x).get(i);
-				if (!visited[y]) {
-					q.offer(y);
-					visited[y] = true;
+		while(!q.isEmpty()) {
+			int a = q.poll();
+			sb.append(a).append(" ");
+			for(int idx = 0; idx<graph.get(a).size(); idx++) {
+				int b = graph.get(a).get(idx);
+				if(!visited[b]) {	// 방문한 적 없으면 큐에 넣기
+					q.offer(b);
+					visited[b] = true;
 				}
 			}
 		}

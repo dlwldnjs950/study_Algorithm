ced131b7887ca8f24dfe6cd80243ab7ce3ee205e
이지원 <dlwldnjs950@naver.com>
2023-08-30 16:10:15 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 134 ms, Memory: 21,104 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,98 +1,96 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-
-	static int[] numbers;	//암호 숫자
-	static int front;
-	static int back;
-	static final int SIZE = 8;	// 암호 숫자는 9개
-
-	// 큐에 넣는 함수
-	static void offer(int insertNum) {
-		if (isFull()) {
-			System.out.println("큐 가득참");
-			return;
-		}
-		numbers[back++ % SIZE] = insertNum;
-	}
-
-	// 큐에서 빼는 함수
-	static int poll() {
-		if (isEmpty())
-			return -1;
-		int tmp = numbers[front % SIZE];
-		numbers[front % SIZE] = 0;
-		front++;
-		return tmp;
-	}
-
-	// 큐가 비었는지 확인하는 함수
-	static boolean isEmpty() {
-		if (front % SIZE == back % SIZE && numbers[front % SIZE] == 0)
-			return true;
-		return false;
-	}
-
-	// 큐가 가득 찼는지 확인하는 함수
-	static boolean isFull() {
-		if (front % SIZE == back % SIZE && numbers[front % SIZE] != 0)
-			return true;
-		return false;
-	}
 	
-	// 맨 마지막에 들어간 원소 반환
-	static int peek() {
-		return numbers[(back-1) % SIZE];
-	}
-
+	/**
+	 * 	# 1225_암호생성기
+	 * 
+	 * 	[설명]
+	 * 	# 암호 생성 방법 (1 사이클)
+	 * 1) 8개의 숫자를 입력 받는다
+	 * 2) 첫번째 숫자를 1감소한 뒤 맨뒤로 보낸다
+	 * 3) 다음 첫번째 숫자를 2 감소한 뒤 맨뒤로
+	 * ...
+	 * 6) 다음 첫번째 숫자를 5감소한 뒤 맨뒤로
+	 * 
+	 * 숫자를 감소시킬 때, 0보다 작아지면 0으로 유지
+	 * 이때, 8자리 숫자 값이 암호가 된다.
+	 * (어떤 한자리가 0이 될 때 까지 반복)
+	 * 
+	 * 10개의 테스트 케이스
+	 * 
+	 * 	[입력]
+	 * ===> 테스트 케이스 번호
+	 * ===> 8개의 번호 
+	 * 
+	 * 	[출력]
+	 * 각 테스트 케이스 별로 만들어진 암호 배열을 출력한다
+	 * 
+	 * 	[풀이방법]
+	 * 1. 앞에서 빼고 뒤로 넣기 때문에 큐 자료구조를 사용한다
+	 * 2. 어떤 한 숫자가 0이 될 때까지 반복한다
+	 * 3. 감소시키는는 숫자는 1-> 2-> 3-> 4->5 순서로 바뀜
+	 * 
+	 * */
+	
 	static BufferedReader br;
 	static StringBuilder sb;
 	static StringTokenizer st;
 	
-	public static void main(String[] args) throws IOException {
+	static final int NUMBERCNT = 8;
+
+	public static void main(String[] args) throws IOException{
+
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
-		// 테스트 케이스는 10개
+		// 테스트 케이스 개수 T
 		int T = 10;
-		
-		for(int testCase = 1; testCase<=T; testCase++) {
+
+		for(int testCase =1; testCase <=T; testCase++) {
 			sb.append("#").append(testCase).append(" ");
-			br.readLine().trim();	// 테스트 케이스 번호 입력은 사용하지 않는다
+
+			// 테스트 케이스 번호 (필요없으므로 받기만 한다)
+			br.readLine();
 			
-			numbers = new int[SIZE];
-			front = 0;
-			back = 0;
-			
+			// 숫자 배열 입력받기
+			Queue<Integer> originNumber = new ArrayDeque<>();
 			st = new StringTokenizer(br.readLine().trim());
-			for(int idx=0; idx<SIZE; idx++) {
-				offer(Integer.parseInt(st.nextToken()));
+			while (st.hasMoreTokens()) {
+				originNumber.offer(Integer.parseInt(st.nextToken()));
 			}
-			// 암호문 생성 한 사이클
-			// "1 감소한 뒤 맨 뒤로, 2 감소한 뒤 맨 뒤로, ... , 5 감소한 뒤 맨뒤로"
-			// 다시 1부터 감소
-			// 어떤 숫자가 작아지다 0보다 작아지면 0으로 유지되며 프로그램 종료
-			int minusNum = 1;
+			
+			int minusNumber =1;
 			while(true) {
-				int tmp = poll()- minusNum;
-				if(tmp<=0) {
-					offer(0);
+				// 맨앞에 있는 숫자를 꺼내서
+				int tmp = originNumber.poll();
+				// 값을 차감하고
+				tmp-= minusNumber++;
+				// 0보다 작아지는 경우엔 0으로 유지하고
+				if(tmp<0)
+					tmp = 0;
+				// 해당 값을 큐에 넣은 다음에
+				originNumber.offer(tmp);
+				
+				// 5를 넘어가면 0 1부터 다시 차감
+				if(minusNumber == 6)
+					minusNumber = 1;
+				// 그 값이 0이 되었다면 반복을 그만한다.
+				if(tmp == 0)
 					break;
-				}
-				offer(tmp);
-				minusNum++;
-				if (minusNum > 5)
-					minusNum = 1;
+				
 			}
-			//System.out.println(Arrays.toString(numbers));
-			while(!isEmpty()) {
-				sb.append(poll()).append(" ");
+			
+			while(!originNumber.isEmpty()) {
+				sb.append(originNumber.poll()).append(" ");
 			}
 			sb.append("\n");
 		}
+		
 		System.out.println(sb);
 	}
 

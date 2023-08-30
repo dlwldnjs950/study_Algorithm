ebbe664751e6e086f8e2bed80fddd4eeaaac3153
이지원 <dlwldnjs950@naver.com>
2023-08-30 21:55:56 +0900

    [Gold V] Title: 탑, Time: 656 ms, Memory: 111876 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,18 +5,32 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	
 	/**
-	 * 	# 2493 탑
+	 * 	#2493 탑
 	 * 
-	 * N개의 높이가 서로 다른 탑
-	 * 수평 직선의 왼쪽부터 오른쪽 방향으로 차례로 세움
-	 * 레이저 신호는 수평 직선의 '왼쪽 방향'으로 발사
-	 * 하나의 탑에서 발사된 레이저 신호는 가장 먼저 만나는 단 하나의 탑에서만 수신 가능
-	 * (가장 가까운 높이가 같거나 큰 탑이 수신)
+	 * 	[ 설명 ]
+	 * 일직선 위에 N개의 높이가 서로 다른 탑
+	 * 레이저 송신기는 수평직선 왼쪽 방향으로 발사
+	 * 가장 먼저 만나는 단 하나의 탑에서만 수신 가능
 	 * 
-	 * 각 탑의 레이저 신호를 어느 탑에서 수신하는 지 알아내라
+	 * 탑들의 개수 N과 높이가 주어질 때,
+	 * 각 탑이 발사한 레이저 신호를 어느 탑에서 수신하는 지 알아내라
 	 * 
+	 * 	[ 입력 ]
+	 * ===> 탑 개수
+	 * ===> 탑 높이
+	 * 
+	 * 	[ 출력 ]
+	 * 각 탑의 신호를 수신한 탑의 번호 출력
+	 * (탑 번호 1번부터 시작)
+	 * 
+	 *  [ 풀이 방법 ]
+	 * 최근에 값 중에 나보다 큰 값이 내 레이저를 받는다
+	 * 1. 스택의 최근 값과 비교해 나보다 크면 기록
+	 * 2. 작으면 pop
+	 * 3. 스택이 비어 비교할 값이 없으면 0
+	 * 4. 다음 비교를 위해 현재 값 스택에 push
 	 * */
 	
 	static BufferedReader br;
@@ -27,29 +41,37 @@ public class Main {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
 		
-		// 탑의 개수 N
-		int N = Integer.parseInt(br.readLine().trim());
-		// 각 탑의 높이를 저장할 배열
-		// 탑의 번호는 1번부터 시작되므로 N+1
-		int[] top = new int[N + 1];
+		// 탑의 개수
+		int topCnt = Integer.parseInt(br.readLine().trim());
 		
-		Stack<Integer> maxHeightIdx = new Stack<>();
+		// 각 탑의 높이
+		// 탑의 번호가 1번부터 시작하므로 +1 크기 배열
+		int topHeight[] = new int[topCnt +1];
+		// 최근값 비교를 위한 자료구조
+		Stack<int[]> stack = new Stack<>();
+		
+		// 해당 인덱스의 레이저를 받은 탑의 인덱스 저장
+		int receiveTop[] = new int[topCnt + 1];
 		st = new StringTokenizer(br.readLine().trim());
-		for (int idx = 1; idx <= N; idx++) {
-			top[idx] = Integer.parseInt(st.nextToken());
-			while(!maxHeightIdx.isEmpty()) {
-				if(top[maxHeightIdx.peek()] >= top[idx]) {
-					sb.append(maxHeightIdx.peek()).append(" ");
+		for(int tIdx = 1; tIdx<=topCnt; tIdx++) {
+			topHeight[tIdx] = Integer.parseInt(st.nextToken());
+			while(!stack.isEmpty()) {
+				if(stack.peek()[1] > topHeight[tIdx]) {
+					receiveTop[tIdx] = stack.peek()[0];
 					break;
-				}
-				maxHeightIdx.pop();
+				}else
+					stack.pop();
 			}
-			if(maxHeightIdx.isEmpty()) {
-				sb.append(0).append(" ");
-			}
-			maxHeightIdx.push(idx);
+			if(stack.isEmpty())
+				receiveTop[tIdx] = 0;
+			stack.push(new int[] {tIdx, topHeight[tIdx]});
+		}
+		
+		for(int tIdx = 1; tIdx<=topCnt; tIdx++) {
+			sb.append(receiveTop[tIdx]).append(" ");
 		}
 		
 		System.out.println(sb);
 	}
+
 }

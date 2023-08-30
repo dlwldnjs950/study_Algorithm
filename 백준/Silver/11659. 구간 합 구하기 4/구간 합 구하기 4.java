9965ca730c9fa63be6aaefb8b6568d63421674b3
이지원 <dlwldnjs950@naver.com>
2023-08-30 09:35:19 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 576 ms, Memory: 56796 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,44 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	
 	/**
-	 * # 구간 합 구하기
-	 * 주어지는 수 N개, i번째 부터 j번째 수까지 합을 구하시오
+	 * 	# 11659_구간 합 구하기4
+	 * 
+	 * 	[설명]
+	 * 수 N개가 주어졌을 때,
+	 * i번째 수부터 j번째 수까지 합을 구하는 프로그램 작성
+	 * 
+	 * 	[입력]
+	 * ===> 수의 개수 N 합을 구해야하는 횟수 M
+	 * ===> N개의 수
+	 * ===> 합을 구해야하는 구간
+	 * 
+	 * 	[출력]
+	 * M개의 줄에 구간 합을 각각 출력
+	 * 
+	 * 	[풀이방법]
+	 * 1. N개의 숫자를 입력받을 때, 숫자의 번호가 1부터 시작하므로 N+1 크기의 배열 선언
+	 * 2. 각 열에는 해당 값 + 이전까지의 누적합을 저장한다
+	 * 3. 주어진 구간에 대한 합을 구할 때,
+	 * 	3-1. 반복문으로 매번 새로 합을 구하면 너무 오래걸린다
+	 * 	3-2. 구간 i~j라면 숫자[j] - 숫자[i-1]을 계산하면
+	 * 		  빼기 연산 만으로 구간 합을 구할 수 있다.
+	 * 
 	 * */
 	
 	static BufferedReader br;
 	static StringBuilder sb;
 	static StringTokenizer st;
-	public static void main(String[] args) throws IOException{
-		// 전역변수 객체 초기화
+
+	public static void main(String[] args) throws IOException {
+
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
-		
-		// 수의 개수 N, 구해야하는 횟수 M
-		String[] NM = br.readLine().trim().split(" ");
-		int N = Integer.parseInt(NM[0]);
-		int M = Integer.parseInt(NM[1]);
-		
-		// N개의 수가 주어진다(공백 구분)
+
+		// 숫자 개수와 구간 합 구할 횟수
 		st = new StringTokenizer(br.readLine().trim());
-		int[] num = new int[N+1];	// i,j가 1부터 시작되는 값이므로 그대로 인덱스 값으로 사용하기 위해 +1 (값을 넣고 볼때 1부터 시작한다)
-		for (int numIdx = 1; numIdx <= N; numIdx++) {
-			// 누적합을 저장한다
-			num[numIdx] = Integer.parseInt(st.nextToken()) + num[numIdx - 1];
+		int numberCnt = Integer.parseInt(st.nextToken());
+		int sumCnt = Integer.parseInt(st.nextToken());
+
+		// N개의 숫자 (누적합 저장)
+		int[] numberSum = new int[numberCnt + 1];
+		st = new StringTokenizer(br.readLine().trim());
+		for (int idx = 1; idx <= numberCnt; idx++) {
+			// 이전까지 누적합 + 해당 값
+			numberSum[idx] = numberSum[idx - 1] + Integer.parseInt(st.nextToken());
 		}
-		// 합을 구하는 구간 i, j가 주어진다 (M 줄)
-		for(int loop = 0; loop < M; loop++) {
-			String[] ij = br.readLine().trim().split(" ");
-			int i = Integer.parseInt(ij[0]);
-			int j = Integer.parseInt(ij[1]);
-			// 반복문으로 합계를 구하면 시간 초과
-			// 배열에 저장할 때 누적합을 저장하여 구간합을  두 누적합에 대한 마이너스 연산으로 구할 수 있도록 한다.
-			sb.append(num[j] - num[i - 1]).append("\n");
+		// 구간 합 구하기
+		for (int sumLoop = 0; sumLoop < sumCnt; sumLoop++) {
+			st = new StringTokenizer(br.readLine().trim());
+			int startIdx = Integer.parseInt(st.nextToken());
+			int endIdx = Integer.parseInt(st.nextToken());
+			sb.append(numberSum[endIdx] - numberSum[startIdx -1]).append("\n");
 		}
+		
 		System.out.println(sb);
 	}
 

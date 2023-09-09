f7db0f0cedd975534311f77a25a84bde48feb298
이지원 <dlwldnjs950@naver.com>
2023-09-09 13:40:41 +0900

    [Bronze IV] Title: 녹색거탑, Time: 76 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static BufferedReader br;
+	static StringBuilder sb;
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException{
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		// N
+		int N = Integer.parseInt(br.readLine().trim());
+
+		sb.append((int)Math.pow(2,N));
+		System.out.println(sb);
+	}
+
+}

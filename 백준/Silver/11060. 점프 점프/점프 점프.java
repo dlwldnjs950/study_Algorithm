98812d6f634b2343fa979b9897152aec7112dd3a
이지원 <dlwldnjs950@naver.com>
2024-01-30 19:22:36 +0900

    [Silver II] Title: 점프 점프, Time: 84 ms, Memory: 11900 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.java
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -1,11 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -33,59 +28,32 @@ public class Main {
 		
 		// 미로 정보
 		int map[] = new int[size + 1];
-		
+		// 최소 점프 저장 변수
+		int minJump[] = new int[size + 1];
+
 		st = new StringTokenizer(br.readLine().trim());
-		
-		for(int idx = 1; idx<= size; idx++) {
+
+		for (int idx = 1; idx <= size; idx++) {
 			map[idx] = Integer.parseInt(st.nextToken());
+			minJump[idx] = 10001;
 		}
-		
-		// int[]에서 0:갈 위치, 1:점프 횟수
-		Queue<int[]> queue = new ArrayDeque<>();
-        
-		boolean visited[] = new boolean[size+1];
-		
-		int minCnt = Integer.MAX_VALUE;
-		
-		visited[1] = true;
-		queue.add(new int[] {1,0});
-		
-		while (!queue.isEmpty()) {
-			
-			int[] current = queue.poll();
-			
-			// 마지막 칸이면 끝내기
-			if(current[0] == size) {
-				minCnt = Math.min(minCnt, current[1]);
-				break;
-			}
-			
-			for(int limit = 1; limit<= map[current[0]]; limit++) {
-				
-				int next = current[0] + limit;
-				
-				// 미로 범위 밖이면 넘어가기
-				if(next > size)
+
+		minJump[1] = 0;
+
+		for (int idx = 1; idx <= size; idx++) {
+			for (int distance = 1; distance <= map[idx]; distance++) {
+				if (idx + distance > size)
 					break;
-				
-				if(visited[next])
-					continue;
-
-				visited[next] = true;
-				queue.add(new int[] { next, current[1] + 1 });
+				minJump[idx + distance] = Math.min(minJump[idx] + 1, minJump[idx + distance]);
 			}
-
 		}
-		
-		// 다 돌고 나왔는데 마지막 칸이 아니면 -1 출력
-		if(minCnt != Integer.MAX_VALUE)
-			sb.append(minCnt);
-		else
-			sb.append(-1);
-		
-		System.out.println(sb);
-		
 
+		if (minJump[size] == 10001)
+			sb.append(-1);
+		else
+			sb.append(minJump[size]);
+
+		System.out.println(sb);
 	}
 
 }

bcae11b1914c87ba629e119a3b433fad4df3c7a5
이지원 <dlwldnjs950@naver.com>
2024-03-24 13:58:57 +0900

    [Silver II] Title: 색종이 만들기, Time: 116 ms, Memory: 13212 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -5,72 +5,57 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
-	/**
-	 * 	# 2630_색종이 만들기
-	 * 
-	 * 	[설명]
-	 * 종이 크기 N×N(N=2^k)
-	 * 
-	 * 	[입력]
-	 * ===> 종이 크기 N
-	 * (N줄의 입력)
-	 * ===> 공간 정보 (공백 구분)
-	 * 
-	 * 	[출력]
-	 * 
-	 * 	[풀이방법]
-	 * 
-	 * */
+	// 시작 칸, 해당 칸 너비
+	// 한가지 색상만 있는지 확인...
 	
 	static BufferedReader br;
 	static StringBuilder sb;
 	static StringTokenizer st;
 	
-	static int N, spaces[][], white, blue;
-
-	public static void main(String[] args) throws IOException{
-
+	static int paperSize, map[][], white, blue;
+	
+	public static void main(String[] args) throws IOException {
+		
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
-		// 공간 크기 N
-		N = Integer.parseInt(br.readLine().trim());
-
-		spaces = new int[N][N];
 		
-		for(int rIdx=0; rIdx< N; rIdx++) {
+		paperSize = Integer.parseInt(br.readLine().trim());
+		
+		map = new int[paperSize][paperSize];
+		
+		for(int rIdx=0; rIdx<paperSize; rIdx++) {
 			st = new StringTokenizer(br.readLine().trim());
-			for(int cIdx=0; cIdx< N; cIdx++) {
-				spaces[rIdx][cIdx] = Integer.parseInt(st.nextToken());
+			for(int cIdx=0; cIdx<paperSize; cIdx++) {
+				map[rIdx][cIdx] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		makingPaper(0, 0, N);
+		separatePaper(0, 0, paperSize);
+		
 		sb.append(white).append("\n").append(blue);
 		System.out.println(sb);
-	}
-	
-	private static void makingPaper(int sRow, int sCol, int size) {
 
+	}
+	private static void separatePaper(int row, int col, int width) {
+		
 		// 해당 공간이 같은 색으로 채워져 있는지 확인
-		int sum = 0;
-		for (int rIdx = sRow; rIdx < sRow + size; rIdx++) {
-			for (int cIdx = sCol; cIdx < sCol + size; cIdx++) {
-				sum += spaces[rIdx][cIdx];
-			}
-		}
-		
-		if(sum == 0) {	// 전부 흰색
-			white++;
-		}else if(sum == size*size) {	// 전부 녹색
-			blue++;
-		}else {	// 섞여있다는 뜻이니까 4분할해서 재귀
-			int half = size >> 1;
-			makingPaper(sRow, sCol, half);
-			makingPaper(sRow, sCol + half, half);
-			makingPaper(sRow + half, sCol, half);
-			makingPaper(sRow + half, sCol + half, half);
-		}
-		
+				int sum = 0;
+				for (int rIdx = row; rIdx < row + width; rIdx++) {
+					for (int cIdx = col; cIdx < col + width; cIdx++) {
+						sum += map[rIdx][cIdx];
+					}
+				}
+				
+				if(sum == 0) {	// 전부 흰색
+					white++;
+				}else if(sum == width*width) {	// 전부 파란색
+					blue++;
+				}else {	// 섞여있다는 뜻이니까 4분할해서 재귀
+					int half = width >> 1;
+					separatePaper(row, col, half);
+					separatePaper(row, col + half, half);
+					separatePaper(row + half, col, half);
+					separatePaper(row + half, col + half, half);
+				}
 	}
-
 }

f255bdbc76814040fb984b7827df3fc8c3cea894
이지원 <dlwldnjs950@naver.com>
2024-06-24 15:01:27 +0900

    [level 2] Title: 전화번호 목록, Time: 1753.63 ms, Memory: 335 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
@@ -11,29 +8,15 @@ class Solution {
             }
         });
 
-        HashMap<String, String> hm = new HashMap<>();
+        HashSet<String> hm = new HashSet<>();
         for (String str : phone_book) {
-            if (hm.get(str) != null) {
+            if (hm.contains(str)) {
                 return false;
             }
             for (int i = 1, len = str.length() ; i <= len; i++) {
-                hm.put(str.substring(0, i), "");
+                hm.add(str.substring(0, i));
             }
         };
         return true;
     }
 }
-//효율성 3,4 실패 코드
-/*Arrays.sort(phone_book, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return o1.length()-o2.length();
-			}
-		});
-		for (int i = 0; i < phone_book.length && answer; i++) {
-			for (int j = i; j < phone_book.length && answer; j++) {
-				if (j!=i && phone_book[i].equals(phone_book[j].substring(0, phone_book[i].length()))) {
-					answer = false;
-				}
-			}
-		}*/

6cc9e386004d79c64f9905db750db4986aa807eb
이지원 <dlwldnjs950@naver.com>
2024-06-27 16:28:09 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.22 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,30 +1,22 @@
 function solution(n, k) {
+    
+    var numbers = Array.from({length:n}, (_,num)=>num + 1);
+    var factorial = [];
+    factorial[0] = 1;
+    factorial[1] = 1;
+    for(let num=2; num<n; num++){
+        factorial[num] = factorial[num-1] * num;
+    }
+    
     var answer = [];
-    var numbers = Array.from({ length: n }, (_, i) => i + 1);
-    // var fact = Array.from({ length: n }, (_, i) => factorial(i));
-    var fact=[];
-    fact[0] = 1;
-    fact[1] = 1;
-    for(let num = 2; num <= n; num++){
-        fact.push(fact[num-1] * num);
+    k--;
+    for(let idx = n-1; idx >=0; idx--){
+         let target = Math.floor(k / factorial[idx]);
+        answer.push(numbers[target]);
+        numbers.splice(target, 1);
+        //console.log(answer);
+        k %= factorial[idx];
     }
-
-    k--;  // k를 0-based 인덱스로 변경
-
-    for (var i = n - 1; i >= 0; i--) {
-        var idx = Math.floor(k / fact[i]);
-        answer.push(numbers[idx]);
-        numbers.splice(idx, 1);  // 사용한 숫자 제거
-        k %= fact[i];
-    }
-
+    
     return answer;
-}
-
-function factorial(n) {
-    var result = 1;
-    for (var i = 2; i <= n; i++) {
-        result *= i;
-    }
-    return result;
 }

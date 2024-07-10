a2df8e9a3006aa5a2bf809381fa3ac1c1eeb0317
이지원 <dlwldnjs950@naver.com>
2024-07-10 13:12:50 +0900

    Update 하노이의 탑.java (주석)

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -11,8 +11,12 @@ class Solution {
     void hanoi(int N, int start, int mid, int end){
         if(N == 0)
             return;
+        // start에 있는 가장 큰 판을 옮기기 위해
+        // start에 있는 N-1개를 end를 임시기둥으로 사용해 mid 기둥에 옮겨두기
         hanoi(N -1, start, end, mid);
         answer.add(new int[]{start, end});
+        // 가장 큰 판을 옮겼으면
+        // mid기둥에 옮겨놨던 N-1개를 end로 옮기기
         hanoi(N-1, mid, start, end);
     }
 }

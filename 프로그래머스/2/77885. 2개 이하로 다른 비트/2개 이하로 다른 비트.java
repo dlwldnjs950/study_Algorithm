9bfb189bc054b111eef09b8af799951240c2b7a4
이지원 <dlwldnjs950@naver.com>
2024-08-13 13:29:36 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 10.08 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+        
+        for(int idx=0; idx < numbers.length; idx++){
+            long number = numbers[idx];
+            // 짝수
+            if(number % 2 == 0){
+                answer[idx] = number +1;
+            // 홀수
+            }else{
+                long tmp = number | number + 1; // 가장 오른쪽 0을 1로 바꿈
+                // 바뀐 가장 오른쪽 0 자리 찾기
+                long change = number ^ tmp;
+                // 그 다음 자리로 옮기기
+                change = change >>> 1;
+                // 0으로 바꾸기 위해 비트 반전
+                change = ~change;
+                // & 연산을 통해 해당 자리 0으로 바꿈
+                tmp = tmp & change;
+                
+                answer[idx] = tmp;
+            }
+        }
+        
+        return answer;
+    }
+}

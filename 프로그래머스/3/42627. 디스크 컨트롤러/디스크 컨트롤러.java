f13a9a36ef4ed1bf1fbb7ca61bf4b59d6733bdc3
이지원 <dlwldnjs950@naver.com>
2024-08-23 14:14:08 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 5.44 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,55 +1,50 @@
 import java.util.*;
 
 class Solution {
-    class Work implements Comparable<Work> {
+    
+    class Task implements Comparable<Task>{
         int request;
         int taken;
-
-        Work(int request, int taken) {
+        
+        Task(int request, int taken){
             this.request = request;
             this.taken = taken;
         }
-
-        public int compareTo(Work o) {
-            return this.taken - o.taken;
+        
+        public int compareTo(Task other){
+            if(this.taken != other.taken)
+                return this.taken - other.taken;
+            else
+                return this.request - other.request;
         }
     }
-
     public int solution(int[][] jobs) {
-        // jobs를 요청 시간 기준으로 정렬
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-
-        Queue<Work> queue = new LinkedList<>(); // 요청 큐
-        PriorityQueue<Work> heap = new PriorityQueue<>(); // 대기 힙 (작업 소요 시간 기준으로 정렬)
-
-        // 요청 큐에 넣기
-        for (int[] job : jobs) {
-            queue.add(new Work(job[0], job[1]));
+        int totalTakenTime = 0;
+        
+        Arrays.sort(jobs, (a,b)-> a[0] - b[0]);
+        Queue<Task> queue = new ArrayDeque<>();
+        PriorityQueue<Task> heap = new PriorityQueue<>();
+        
+        for(int[] job : jobs){
+            queue.add(new Task(job[0],job[1]));
         }
-
+        
         int currentTime = 0;
-        int totalWaitingTime = 0;
-        int count = jobs.length;
-
-        while (!queue.isEmpty() || !heap.isEmpty()) {
-            // 현재 시점에서 처리 가능한 모든 작업을 대기 힙에 추가
-            // 요청 시간이 같은 작업이 여러개일수도 있으니까
-            // 이것도 힙에 넣어서 순서를 정해서 작업해야함
-            while (!queue.isEmpty() && queue.peek().request <= currentTime) {
+        while(!queue.isEmpty() || !heap.isEmpty()){
+            
+            while(!queue.isEmpty() && queue.peek().request <= currentTime){
                 heap.add(queue.poll());
             }
-
-            // 대기 힙에서 작업을 처리
-            if (!heap.isEmpty()) {
-                Work current = heap.poll();
+            
+            if(!heap.isEmpty()){
+                Task current = heap.poll();
                 currentTime += current.taken;
-                totalWaitingTime += currentTime - current.request;
-            } else if (!queue.isEmpty()) {
-                // 대기 힙이 비어있다면, 다음 작업의 요청 시간으로 이동
+                totalTakenTime += currentTime - current.request;
+            }else if(!queue.isEmpty()){
                 currentTime = queue.peek().request;
             }
         }
-
-        return totalWaitingTime / count;
+        
+        return totalTakenTime / jobs.length;
     }
 }

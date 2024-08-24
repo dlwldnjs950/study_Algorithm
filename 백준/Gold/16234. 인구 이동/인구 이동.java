f27e2cdc16fe5f242905de4a461798a17c3bea5d
이지원 <dlwldnjs950@naver.com>
2024-08-24 23:28:13 +0900

    [Gold IV] Title: 인구 이동, Time: 488 ms, Memory: 293500 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,103 +1,153 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
+	
+	/**
+	 * 	# 16234_인구이동
+	 * 
+	 * 	[설명]
+	 * NxN 땅
+	 * r행 c열 인구 주어짐
+	 * 인접한 나라 국경선
+	 * 
+	 * 하루동안 다음과 같은 인구 이동
+	 * 1. 인구 차이가 L이상 R이하 국경선 열림
+	 * 2. 국경선 모두 열었다면 인구 이동
+	 * 3. 인구수는 (연합의 인구수) / (연합을 이루고 있는 칸의 개수) 소수점 버림
+	 * 
+	 * 인구 이동 며칠동안?
+	 * 
+	 * 	[입력]
+	 * ===> 땅크기 차이최소 차이최대
+	 * 		(N개의 줄) 각 나라의 인구수
+	 * 
+	 * 	[출력]
+	 * 인구 이동 며칠동안 발생
+	 * 
+	 * 	[풀이방법]
+	 * 2차원 배열에 인구 수 저장
+	 * 그래프 탐색으로 연합 구성
+	 * 연합 구성 후 인구 이동 
+	 * 
+	 * */
+	
+	static class Point{
+		int row;
+		int col;
+		
+		Point(int row, int col){
+			this.row = row;
+			this.col = col;
+		}
+	}
+	
 	static BufferedReader br;
-	static StringBuilder sb;
 	static StringTokenizer st;
-
+	
 	static int mapSize, minDiff, maxDiff, map[][];
-	static boolean isVisited[][];
-	static List<int[]> list;
+	static List<Point> openCountry = new ArrayList<>();
+	static boolean[][] visited;
+	
+	public static void main(String[] args) throws IOException{
 
-	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-
+		
 		st = new StringTokenizer(br.readLine().trim());
 		mapSize = Integer.parseInt(st.nextToken());
 		minDiff = Integer.parseInt(st.nextToken());
 		maxDiff = Integer.parseInt(st.nextToken());
-
+		
+		// 인구 정보 저장
 		map = new int[mapSize][mapSize];
-		for (int rIdx = 0; rIdx < mapSize; rIdx++) {
+		for(int rIdx = 0; rIdx < mapSize; rIdx++) {
 			st = new StringTokenizer(br.readLine().trim());
-			for (int cIdx = 0; cIdx < mapSize; cIdx++) {
+			
+			for(int cIdx = 0; cIdx < mapSize; cIdx++) {
 				map[rIdx][cIdx] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		int result = 0;
-		while (true) {
+		
+		// 인구 이동 날짜 (결과값)
+		int movingDay = 0;
+		while(true){		
+			
+			visited = new boolean[mapSize][mapSize];
 			boolean isMove = false;
-			isVisited = new boolean[mapSize][mapSize];
-			for (int rIdx = 0; rIdx < mapSize; rIdx++) {
-				for (int cIdx = 0; cIdx < mapSize; cIdx++) {
-					if (!isVisited[rIdx][cIdx]) {
-						int sum = BFS(rIdx, cIdx);
-						if (list.size() > 1) {
-							populationMove(sum);
+			
+			// 각 나라에서 연합 구성
+			for(int rIdx = 0; rIdx < mapSize; rIdx++) {
+				for(int cIdx = 0; cIdx < mapSize; cIdx++) {
+					
+					if(!visited[rIdx][cIdx]) {
+						// 한 연합에 대해 인구 이동 실시
+						int populSum = bfs(rIdx, cIdx);
+						if(openCountry.size() > 1) {
+							movePopul(populSum);
 							isMove = true;
 						}
 					}
 				}
 			}
-			if (!isMove)
+			
+			if(!isMove)
 				break;
-			result++;
+			movingDay++;			
 		}
-
-		sb.append(result);
-		System.out.println(sb);
-
+		
+		System.out.println(movingDay);
 	}
 
-	static int[] dr = { 1, 0, -1, 0 }, dc = { 0, 1, 0, -1 };
-
-	public static int BFS(int row, int col) {
-		Queue<int[]> queue = new ArrayDeque<>();
-		list = new ArrayList<int[]>();
-
-		queue.offer(new int[] { row, col });
-		list.add(new int[] { row, col });
-		isVisited[row][col] = true;
-
-		int sum = map[row][col];
-
-		while (!queue.isEmpty()) {
-			int[] current = queue.poll();
-
-			for (int direction = 0; direction < dr.length; direction++) {
-				int nr = current[0] + dr[direction];
-				int nc = current[1] + dc[direction];
-				if (nr < 0 || nc < 0 || nr >= mapSize || nc >= mapSize || isVisited[nr][nc])
+	private static int bfs(int row, int col) {
+		
+		openCountry.clear();
+		int populSum = 0;
+		
+		int[] dr = {0, 1, 0, -1}, dc = {1, 0, -1, 0};
+		
+		Queue<Point> queue = new ArrayDeque<>();
+		
+		queue.add(new Point(row, col));
+		visited[row][col] = true;
+		
+		while(!queue.isEmpty()) {
+			
+			Point current = queue.poll();
+			// 연합에 추가
+			openCountry.add(current);
+			int popul = map[current.row][current.col];
+			// 연합 총 인구 수
+			populSum += popul;
+			
+			for(int direction = 0; direction < dr.length; direction++) {
+				int nr = current.row + dr[direction];
+				int nc = current.col + dc[direction];
+				
+				if(nr < 0 || nc < 0 || nr >= mapSize || nc >= mapSize)
 					continue;
-				int diff = Math.abs(map[current[0]][current[1]] - map[nr][nc]);
-				if (minDiff <= diff && diff <= maxDiff) {
-					queue.offer(new int[] { nr, nc });
-					list.add(new int[] { nr, nc });
-					sum += map[nr][nc];
-					isVisited[nr][nc] = true;
+				if(visited[nr][nc])
+					continue;
+				
+				// 인구 차이가 조건에 맞으면 국경 열림
+				int diff = Math.abs(popul - map[nr][nc]);
+				if (diff >= minDiff && diff <= maxDiff) {
+					queue.add(new Point(nr, nc));
+					visited[nr][nc] = true;
 				}
 			}
-		}
-
-		return sum;
+		}		
+		
+		return populSum;
 	}
 
-	private static void populationMove(int sum) {
-		int avg = sum / list.size();
-		for(int[] city : list) {
-			map[city[0]][city[1]] = avg;
+	private static void movePopul(int populSum) {
+		
+		int changePopul = populSum / openCountry.size();
+		
+		for(Point country : openCountry) {
+			map[country.row][country.col] = changePopul;
 		}
-
 	}
 
+	
 }

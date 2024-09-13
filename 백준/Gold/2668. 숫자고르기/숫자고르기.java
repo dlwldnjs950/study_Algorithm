86457238fdef8bb88e42b742d26fece2833bbdb8
이지원 <dlwldnjs950@naver.com>
2024-09-13 12:47:45 +0900

    [Gold V] Title: 숫자고르기, Time: 64 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,68 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	/*
-	 * 세로 2줄, 가로 N칸
-	 * 
-	 * 첫째줄 : 1 ~ N이 차례대로
-	 * 둘째줄 : 1 이상 N 이하 정수 (무작위)
-	 * 		첫째줄에서 숫자를 적절히 뽑으면
-	 * 		그 뽑힌 정수 집합과 뽑힌 정수 바로 밑 둘째줄 정수 집합 일치
-	 * 	최대로 많이 뽑기
-	 * 
-	 * 앞에서부터 순서대로 뽑/안뽑
-	 * 다 선택 되었을 때, 선택인덱스 집합과 선택값 집합이 같은지 확인
-	 * */
-	
+	/**/
+
 	static BufferedReader br;
 	static StringBuilder sb;
 	static StringTokenizer st;
-	
-	static boolean isSelected[];
-	static int N, grid[];
-	static List<Integer> result = new ArrayList<Integer>();
-	
+
+	static List<Integer> answer;
+
 	public static void main(String[] args) throws IOException {
+
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
-		
-		N = Integer.parseInt(br.readLine().trim());
-		grid = new int[N +1];
-		isSelected = new boolean[N +1];
-		
-		for(int idx=1; idx<=N; idx++) {
-			grid[idx] = Integer.parseInt(br.readLine().trim());
-		}
-		
-		for(int idx=1; idx<=N; idx++) {
-			isSelected[idx] = true;
-			dfs(idx, idx);
-			isSelected[idx] = false;
+
+		int numNum = Integer.parseInt(br.readLine().trim());
+
+		int numbers[] = new int[numNum + 1];
+		for (int idx = 1; idx <= numNum; idx++) {
+			numbers[idx] = Integer.parseInt(br.readLine().trim());
 		}
 
-		Collections.sort(result);
-		sb.append(result.size()).append("\n");
-		for (int num : result)
-			sb.append(num).append("\n");
+		answer = new ArrayList<>();
+		boolean[] isVisited = new boolean[numNum + 1];
+		for (int idx = 1; idx <= numNum; idx++) {
+			// 각 인덱스로 출발해서 다시 인덱스로 돌아올 수 있는지
+			isVisited[idx] = true;
+			dfs(idx, idx, numbers, isVisited);
+			isVisited[idx] = false;
+		}
 
+		Collections.sort(answer);
+		sb.append(answer.size());
+		for(int idx : answer) {
+			sb.append("\n").append(idx);
+		}
+		
 		System.out.println(sb);
+
 	}
 
-	static void dfs(int startIdx, int targetIdx) {
-		if (isSelected[grid[startIdx]] == false) {
-			isSelected[grid[startIdx]] = true;
-			dfs(grid[startIdx], targetIdx);
-			isSelected[grid[startIdx]] = false;
+	private static void dfs(int startIdx, int targetIdx, int[] numbers, boolean[] isVisited) {
+
+		int nextIdx = numbers[startIdx];
+
+		if (isVisited[nextIdx]) {
+			if (nextIdx == targetIdx)
+				answer.add(targetIdx);
+			return;
 		}
-		if(grid[startIdx] == targetIdx)
-			result.add(targetIdx);
+
+		isVisited[nextIdx] = true;
+		dfs(nextIdx, targetIdx, numbers, isVisited);
+		isVisited[nextIdx] = false;
+
 	}
+
 }

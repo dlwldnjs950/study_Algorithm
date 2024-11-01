a0b5fa3ba238a8fd793969a7896ccbd043547547
이지원 <dlwldnjs950@naver.com>
2024-11-01 14:44:24 +0900

    [Silver III] Title: 반복하지 않는 수, Time: 684 ms, Memory: 41804 KB -BaekjoonHub

--- 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.java
+++ 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.java
@@ -1,46 +1,44 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
+    static List<Integer> result = new ArrayList<>();
+    static boolean[] used = new boolean[10];
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		int[] e = new int[1000001];
-		int count = 1;
-		int i = 1;
-		boolean[] g;
-		while(true) {
-			if(count == 1000001) break;
-			boolean check = false;
-			int copy = i;
-			g = new boolean[10];
-			while(copy > 0) {
-				if(g[copy % 10]) {
-					check = true;
-					break;
-				}
-				g[copy % 10] = true;
-				copy /= 10;
-			}
-			i++;
-			if(!check) {
-				e[count] = i;
-				count++;
-			} 
-			if(i % 100000 == 0) System.gc();
-		}
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		while(true) {
-			int a = Integer.parseInt(br.readLine());
-			if(a == 0) break;
-			sb.append(e[a] - 1 + "\n");
-		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        precompute();
+
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+            sb.append(result.get(n - 1)).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static void precompute() {
+        int num = 1;
+        while (result.size() < 1000000) {
+            if (isUnique(num)) {
+                result.add(num);
+            }
+            num++;
+        }
+    }
+
+    static boolean isUnique(int n) {
+    	Arrays.fill(used, false);
+        
+        while (n > 0) {
+            int digit = n % 10;
+            if (used[digit]) return false;
+            used[digit] = true;
+            n /= 10;
+        }
+        return true;
+    }
 }
